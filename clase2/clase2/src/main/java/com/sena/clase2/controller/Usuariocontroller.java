58b4a4f96b0b6c3e3d449e067c3bb97e6a4ac827
anderson001-ops <joelcapera2023@gmail.com>
2025-08-13 02:20:37 -0500

    nuevo arreglo

--- clase2/clase2/src/main/java/com/sena/clase2/controller/Usuariocontroller.java
+++ clase2/clase2/src/main/java/com/sena/clase2/controller/Usuariocontroller.java
@@ -5,14 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sena.clase2.Repositoryes.UsuarioRepositorie;
 import com.sena.clase2.models.Usuario;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 
 
@@ -32,20 +32,20 @@ private UsuarioRepositorie UserRepo;
     return UserRepo.findAll().toString();
     }
     @GetMapping("/listausuario")
-    public  List<Usuario> Usuariolista(){
+    public  List<Usuario> usuariolista(){
         return UserRepo. findAll();
     }
 @PostMapping("/agregarusuario")
-public Usuario Crearusuario(@RequestBody Usuario usuario) {
+public Usuario crearusuario(@RequestBody Usuario usuario) {
     return UserRepo.save(usuario);
 }
 
-@DeleteMapping("/usuario/{id}")
-public String eliminarusuario(@pathVariable Integer id ){
+@DeleteMapping("/eliminarusuario/{id}")
+public String eliminarusuario(@PathVariable Integer id ){
     UserRepo.deleteById(id);
-    return "eliminar usu";
+    return "se elimino usuario";
 }
-@PutMapping("usuario/{id}")
+@PutMapping("verusuario/{id}")
 public String userupdate(@PathVariable String id, @RequestBody Usuario usuario ) {
     Usuario userupdate = UserRepo.findById(Integer.parseInt(id)).orElse(null);
     if (userupdate == null){
